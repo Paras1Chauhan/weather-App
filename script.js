086035cf7ab996ffafb76495f69c844df4ba7d03
Paras1Chauhan <paraschauhan05112001@gmail.com>
2024-02-08 12:57:42 +0530

    Add Initial Files

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a3aba249f4msh9367b20780e886dp126cb8jsn1a5cfa6d96ec',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
